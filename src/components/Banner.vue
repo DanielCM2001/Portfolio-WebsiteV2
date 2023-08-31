d4e86faf18c0c8175809ae3bdc01c9c2edab2e87
danyczmartins <dani.martins2001@hotmail.com>
2023-08-31 12:54:55 +0100

    Banner

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,6 +1,20 @@
-<template>
-  <div class="bg-gray-800 text-3xl min-h-screen flex">
+<!-- <template>
+  <div class="bg-gray-800 text-3xl min-h-screen">
     <div class="bg-gray-900 flex justify-center items-center">hi</div>
+  </div>
+</template>
+ -->
+
+<template>
+  <div
+    class="bg-gray-800 text-3xl min-h-screen flex justify-center items-center"
+  >
+    <div
+      class="flex justify-center items-center bg-gray-900 w-full flex-col md:flex-row"
+    >
+      <div class="w-1/2 bg-gray-500">hi</div>
+      <div class="w-1/2 bg-gray-600">.</div>
+    </div>
   </div>
 </template>
 
