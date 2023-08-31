dcc6333ccb3e881c00dc44425be021ac0502611a
danyczmartins <dani.martins2001@hotmail.com>
2023-08-31 23:34:00 +0100

    Update Banner

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,19 +1,22 @@
-<!-- <template>
-  <div class="bg-gray-800 text-3xl min-h-screen">
-    <div class="bg-gray-900 flex justify-center items-center">hi</div>
-  </div>
-</template>
- -->
-
 <template>
-  <div
-    class="bg-gray-800 text-3xl min-h-screen flex justify-center items-center"
-  >
+  <div class="min-h-screen flex justify-center items-center">
     <div
-      class="flex justify-center items-center bg-gray-900 w-full flex-col md:flex-row"
+      class="flex justify-center items-center w-full flex-col md:flex-row space-y-20"
     >
-      <div class="w-1/2 bg-gray-500">hi</div>
-      <div class="w-1/2 bg-gray-600">.</div>
+      <div class="w-full lg:w-2/3">
+        <h1 class="text-5xl">Hi there! I'm Daniel</h1>
+        <p class="text-2xl">
+          A front-end Web Developer passionate about creating interactive
+          applications and experiences on the web.
+        </p>
+      </div>
+      <div class="w-full lg:w-1/3 flex justify-end">
+        <img
+          src="../assets/img/BannerRectangle.png"
+          alt="Banner image"
+          class="w-full max-w-lg w-80"
+        />
+      </div>
     </div>
   </div>
 </template>
