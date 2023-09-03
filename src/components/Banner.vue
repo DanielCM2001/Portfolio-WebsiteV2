6958ff47a0b73cebd1eddf680f71f6571f71a849
danyczmartins <dani.martins2001@hotmail.com>
2023-09-03 17:10:37 +0100

    banner and font

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -4,21 +4,26 @@
       class="flex justify-center items-center w-full flex-col md:flex-row space-y-20"
     >
       <div
-        class="w-full lg:w-2/3 bg-slate-600 flex justify-center items-center"
+        class="w-full lg:w-1/2 bg-slate-600 flex justify-center items-center flex-col"
       >
-        <div class="w-1/3 space-y-10 bg-slate-700">
+        <div class="w-1/2 space-y-10 bg-slate-700">
           <h1 class="text-5xl">Hi there! I'm Daniel</h1>
           <p class="text-2xl">
             A front-end Web Developer passionate about creating interactive
             applications and experiences on the web.
           </p>
+
+          <div class="flex flex-row space-x-10">
+            <button>1</button>
+            <button>2</button>
+          </div>
         </div>
       </div>
-      <div class="w-full lg:w-1/3 flex justify-end">
+      <div class="w-full lg:w-1/2 flex justify-end">
         <img
           src="../assets/img/BannerRectangle.png"
           alt="Banner image"
-          class="w-full max-w-lg"
+          class="w-full max-w-2xl"
         />
       </div>
     </div>
