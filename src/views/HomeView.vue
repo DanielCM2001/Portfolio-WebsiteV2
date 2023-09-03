4c4493fd6b3eaf3522018f060d3a0dbe96532e9f
danyczmartins <dani.martins2001@hotmail.com>
2023-09-03 23:59:39 +0100

    DesignerCoder

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <script setup>
 import TheWelcome from "../components/TheWelcome.vue";
 import Banner from "../components/Banner.vue";
+import DesignerCoder from "../components/DesignerCoder.vue";
 </script>
 
 <template>
   <main>
     <TheWelcome />
     <Banner />
+    <DesignerCoder />
   </main>
 </template>
