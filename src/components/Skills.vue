9532196e30aa96357afd777824cdd5f9c27063fa
danyczmartins <dani.martins2001@hotmail.com>
2023-09-06 16:38:46 +0100

    Skills mobile first draw

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -19,6 +19,48 @@
     <div class="w-0 md:w-1/4 flex justify-center items-center">linha</div>
     <div class="w-full md:w-3/4 bg-gray-900">
       <h1 class="text-4xl">What I'm really good at</h1>
+
+      <div class="flex flex-col justify-center items-center space-y-10 h-96">
+        <!--   <div class="box">
+          <div class="flex flex-col justify-center items-center py-8 px-8">
+            <img src="../assets/img/VueIcon.png" class="w-24" />
+            <h1 class="text-xl">Vue JS</h1>
+          </div>
+        </div>
+
+        <div class="box">
+          <div class="flex flex-col justify-center items-center py-8 px-8">
+            <img src="../assets/img/ReactIcon.png" class="w-24" />
+            <h1 class="text-xl">Vue JS</h1>
+          </div>
+        </div>
+        <div class="box">
+          <div class="flex flex-col justify-center items-center py-8 px-8">
+            <img src="../assets/img/VueIcon.png" class="w-24" />
+            <h1 class="text-xl">Vue JS</h1>
+          </div>
+        </div>
+
+        <div class="box">
+          <div class="flex flex-col justify-center items-center py-8 px-8">
+            <img src="../assets/img/ReactIcon.png" class="w-24" />
+            <h1 class="text-xl">Vue JS</h1>
+          </div>
+        </div> -->
+
+        <div class="box">
+          <div class="flex flex-col justify-center items-center py-8 px-8">
+            <img src="../assets/img/VueIcon.png" class="w-28" />
+            <h1 class="text-xl">Vue JS</h1>
+          </div>
+        </div>
+        <div class="box">
+          <div class="flex flex-col justify-center items-center py-8 px-8">
+            <img src="../assets/img/VueIcon.png" class="w-44" />
+            <h1 class="text-xl">Vue JS</h1>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -34,7 +76,7 @@ export default {
 <style scoped>
 .box {
   border-radius: 40px;
-  background: #313339;
-  box-shadow: 0px 5px 5px 0px #8c5dc9 inset, 0px 4px 40px 0px #c940eb;
+  background: #2d2d31;
+  box-shadow: 0px 3px 3px 0px #8c5dc9 inset, 0px 4px 40px 0px #c940eb;
 }
 </style>
