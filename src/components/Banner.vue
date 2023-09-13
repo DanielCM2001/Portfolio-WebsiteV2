354efde9f4b9f1eed401726dacb1255f3a025f02
danyczmartins <dani.martins2001@hotmail.com>
2023-09-13 14:47:58 +0100

    Banner updates

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,177 +1,3 @@
-<!-- <template>
-  <div class="flex items-center justify-center min-h-screen">
-    <div class="flex flex-col items-center justify-center w-full md:flex-row">
-      <div class="flex flex-col items-center justify-center w-full lg:w-1/2">
-        <div class="w-full space-y-10 lg:w-1/2">
-          <h1 class="text-5xl">Hi there! I'm Daniel</h1>
-          <p class="text-2xl">
-            A front-end Web Developer passionate about creating interactive
-            applications and experiences on the web.
-          </p>
-
-          <div class="flex flex-row space-x-10 text-lg">
-            <button class="px-8 py-3 primaryButtons">LinkedIn</button>
-            <button class="px-5 py-3 primaryButtons">Icon</button>
-            <button class="px-5 py-3 primaryButtons">Icon</button>
-          </div>
-        </div>
-      </div>
-      <div class="flex justify-end w-full lg:w-1/2">
-        <img
-          src="../assets/img/BannerRectangle.png"
-          alt="Banner image"
-          class="w-full max-w-2xl"
-        />
-      </div>
-    </div>
-  </div>
-</template>
-
-<script>
-export default {
-  setup() {
-    return {};
-  },
-};
-</script>
-
-<style scoped>
-.primaryButtons {
-  border-radius: 20px;
-  background: #c940eb;
-  box-shadow:
-    0px 5px 5px 0px #8c5dc9 inset,
-    0px 4px 50px 0px #c940eb;
-}
-</style>
- -->
-
-<!-- <template>
-  <div class="flex items-center justify-center min-h-screen">
-    <div class="flex flex-col items-center justify-center w-full md:flex-row">
-      <div class="flex flex-col items-center justify-center w-full lg:w-1/2">
-        <div class="w-full space-y-10 mt-60 lg:w-1/2">
-          <h1 class="text-5xl">Hi there! I'm Daniel</h1>
-          <p class="text-2xl">
-            A front-end Web Developer passionate about creating interactive
-            applications and experiences on the web.
-          </p>
-
-          <div class="flex flex-row space-x-10 text-lg">
-            <button class="px-8 py-3 primaryButtons">LinkedIn</button>
-            <button class="px-5 py-3 primaryButtons">Icon</button>
-            <button class="px-5 py-3 primaryButtons">Icon</button>
-          </div>
-        </div>
-        <div class="w-full mt-48 lg:w-1/2">
-          <p class="text-2xl">
-            A front-end Web Developer passionate about creating interactive
-            applications and experiences on the web.
-          </p>
-        </div>
-      </div>
-      <div class="flex justify-end w-full lg:w-1/2">
-        <img
-          src="../assets/img/BannerRectangle.png"
-          alt="Banner image"
-          class="w-full max-w-2xl"
-        />
-      </div>
-    </div>
-  </div>
-</template>
-
-<script>
-export default {
-  setup() {
-    return {};
-  },
-};
-</script>
-
-<style scoped>
-.primaryButtons {
-  border-radius: 20px;
-  background: #c940eb;
-  box-shadow:
-    0px 5px 5px 0px #8c5dc9 inset,
-    0px 4px 50px 0px #c940eb;
-}
-</style>
- -->
-
-<!-- <template>
-  <div class="flex items-center justify-center min-h-screen">
-    <div class="flex flex-col items-center justify-center w-full md:flex-row">
-      <div class="flex flex-col items-center justify-center w-full lg:w-1/2">
-        <div class="w-full space-y-10 mt-52 lg:w-1/2">
-          <h1 class="text-5xl">Hi there! I'm Daniel</h1>
-          <p class="text-2xl">
-            A front-end Web Developer passionate about creating interactive
-            applications and experiences on the web.
-          </p>
-
-          <div class="flex flex-row space-x-10 text-lg">
-            <button class="px-8 py-3 primaryButtons">LinkedIn</button>
-            <button class="px-5 py-3 primaryButtons">Icon</button>
-            <button class="px-5 py-3 primaryButtons">Icon</button>
-          </div>
-        </div>
-        <div class="w-full mt-40 lg:w-1/2">
-          <div class="flex flex-col px-10 py-6 lg:flex-row InfoBox">
-            <div class="flex flex-row w-full space-x-5 lg:w-1/3 lg:flex-col">
-              <p class="text-5xl">+5</p>
-              <p class="text-lg">Lorem Ipsum dolor</p>
-            </div>
-            <div class="flex flex-row w-full space-x-5 lg:w-1/3 lg:flex-col">
-              <p class="text-5xl">+5</p>
-              <p class="text-lg">Lorem Ipsum dolor</p>
-            </div>
-            <div class="flex flex-row w-full space-x-5 lg:w-1/3 lg:flex-col">
-              <p class="text-5xl">+5</p>
-              <p class="text-lg">Lorem Ipsum dolor</p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="flex justify-end w-full lg:w-1/2">
-        <img
-          src="../assets/img/BannerRectangle.png"
-          alt="Banner image"
-          class="w-full max-w-2xl"
-        />
-      </div>
-    </div>
-  </div>
-</template>
-
-<script>
-export default {
-  setup() {
-    return {};
-  },
-};
-</script>
-
-<style scoped>
-.primaryButtons {
-  border-radius: 20px;
-  background: #c940eb;
-  box-shadow:
-    0px 5px 5px 0px #8c5dc9 inset,
-    0px 4px 50px 0px #c940eb;
-}
-
-.InfoBox {
-  border-radius: 30px;
-  background: #242428;
-  box-shadow:
-    0px 5px 5px 0px #8c5dc9 inset,
-    0px 4px 50px 0px #c940eb;
-}
-</style>
- -->
-
 <template>
   <div class="flex flex-col items-center justify-center min-h-screen">
     <div class="flex flex-col items-center justify-center w-full md:flex-row">
@@ -183,58 +9,29 @@ export default {
             applications and experiences on the web.
           </p>
 
-          <div class="flex flex-row space-x-10 text-lg">
+          <div class="flex flex-row space-x-5 text-lg sm:space-x-10">
             <button class="px-8 py-3 primaryButtons">LinkedIn</button>
             <button class="px-5 py-3 primaryButtons">Icon</button>
             <button class="px-5 py-3 primaryButtons">Icon</button>
           </div>
         </div>
-      </div>
-      <div class="flex justify-end w-full lg:w-1/2">
-        <img
-          src="../assets/img/BannerRectangle.png"
-          alt="Banner image"
-          class="w-full max-w-2xl"
-        />
-      </div>
-    </div>
-    <!--  <div
-      class="flex flex-col items-center justify-center w-full -translate-y-64"
-    >
-      <div class="w-full mt-40 lg:w-1/2">
-        <div class="flex flex-col px-10 py-6 lg:flex-row InfoBox">
-          <div class="flex flex-row w-full space-x-5 lg:w-1/3 lg:flex-col">
-            <p class="text-5xl">+5</p>
-            <p class="text-lg">Lorem Ipsum dolor</p>
-          </div>
-          <div class="flex flex-row w-full space-x-5 lg:w-1/3 lg:flex-col">
-            <p class="text-5xl">+5</p>
-            <p class="text-lg">Lorem Ipsum dolor</p>
-          </div>
-          <div class="flex flex-row w-full space-x-5 lg:w-1/3 lg:flex-col">
-            <p class="text-5xl">+5</p>
-            <p class="text-lg">Lorem Ipsum dolor</p>
-          </div>
-        </div>
-      </div>
-    </div> -->
 
-    <!-- <div class="flex flex-col items-center justify-center w-full md:flex-row">
-      <div class="flex flex-col items-center justify-center w-full lg:w-1/2">
-        <div class="w-full space-y-10 lg:w-1/2">
-          <div class="flex flex-col px-10 py-6 lg:flex-row InfoBox">
-            <div class="flex flex-row w-full space-x-5 lg:w-1/3 lg:flex-col">
-              <p class="text-5xl">+5</p>
-              <p class="text-lg">Lorem Ipsum dolor</p>
-            </div>
-            <div class="flex flex-row w-full space-x-5 lg:w-1/3 lg:flex-col">
-              <p class="text-5xl">+5</p>
-              <p class="text-lg">Lorem Ipsum dolor</p>
-            </div>
-            <div class="flex flex-row w-full space-x-5 lg:w-1/3 lg:flex-col">
-              <p class="text-5xl">+5</p>
-              <p class="text-lg">Lorem Ipsum dolor</p>
-            </div>
+        <div class="flex flex-col px-10 py-6 space-y-5 InfoBox">
+          <div class="flex flex-row w-full space-x-5">
+            <p class="text-5xl">+5</p>
+            <p class="text-lg">Lorem Ipsum dolor</p>
+          </div>
+          <div class="w-full h-1 separatorBar"></div>
+          <!-- Horizontal bar with #C940EB color -->
+          <div class="flex flex-row w-full space-x-5">
+            <p class="text-5xl">60%</p>
+            <p class="text-lg">Lorem Ipsum dolor</p>
+          </div>
+          <div class="w-full h-1 separatorBar"></div>
+          <!-- Horizontal bar with #C940EB color -->
+          <div class="flex flex-row w-full space-x-5">
+            <p class="text-5xl">100</p>
+            <p class="text-lg">Lorem Ipsum dolor</p>
           </div>
         </div>
       </div>
@@ -244,31 +41,6 @@ export default {
           alt="Banner image"
           class="w-full max-w-2xl"
         />
-      </div>
-    </div> -->
-
-    <div class="w-full bg-gray-600">
-      <div
-        class="flex flex-col items-center justify-center w-full bg-gray-700 lg:w-1/2"
-      >
-        <div
-          class="w-full bg-gray-800 lg:w-3/4 lg:ml-28 xl:w-3/5 xl:ml-20 2xl:w-1/2 2xl:ml-0"
-        >
-          <div class="flex flex-col px-10 py-6 lg:flex-row InfoBox">
-            <div class="flex flex-row w-full space-x-5 lg:w-1/3 lg:flex-col">
-              <p class="text-5xl">+5</p>
-              <p class="text-lg">Lorem Ipsum dolor</p>
-            </div>
-            <div class="flex flex-row w-full space-x-5 lg:w-1/3 lg:flex-col">
-              <p class="text-5xl">+5</p>
-              <p class="text-lg">Lorem Ipsum dolor</p>
-            </div>
-            <div class="flex flex-row w-full space-x-5 lg:w-1/3 lg:flex-col">
-              <p class="text-5xl">+5</p>
-              <p class="text-lg">Lorem Ipsum dolor</p>
-            </div>
-          </div>
-        </div>
       </div>
     </div>
   </div>
@@ -292,7 +64,15 @@ export default {
 }
 .InfoBox {
   border-radius: 30px;
-  background: #242428;
+  background: #151417;
+  box-shadow:
+    0px 5px 5px 0px #8c5dc9 inset,
+    0px 4px 50px 0px #c940eb;
+}
+
+.separatorBar {
+  border-radius: 20px;
+  background: #c940eb;
   box-shadow:
     0px 5px 5px 0px #8c5dc9 inset,
     0px 4px 50px 0px #c940eb;
