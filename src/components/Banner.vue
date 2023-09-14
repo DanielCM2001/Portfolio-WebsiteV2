3d761af707e77a61501b44cc70402baf6b536b48
danyczmartins <dani.martins2001@hotmail.com>
2023-09-14 01:29:55 +0100

    Update Banner.vue

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,4 +1,24 @@
-<template>
+<!--   <div
+          class="flex flex-col px-10 py-6 space-y-5 lg:flex-row lg:space-y-0 InfoBox"
+        >
+          <div class="flex flex-row w-full space-x-5">
+            <p class="text-5xl">+5</p>
+            <p class="text-lg">Lorem Ipsum dolor</p>
+          </div>
+          <div class="w-full h-1 separatorBar"></div>
+          <div class="flex flex-row w-full space-x-5">
+            <p class="text-5xl">60%</p>
+            <p class="text-lg">Lorem Ipsum dolor</p>
+          </div>
+          <div class="w-full h-1 separatorBar"></div>
+
+          <div class="flex flex-row w-full space-x-5">
+            <p class="text-5xl">100</p>
+            <p class="text-lg">Lorem Ipsum dolor</p>
+          </div>
+        </div> -->
+
+<!-- <template>
   <div class="flex flex-col items-center justify-center min-h-screen">
     <div class="flex flex-col items-center justify-center w-full md:flex-row">
       <div class="flex flex-col items-center justify-center w-full lg:w-1/2">
@@ -16,20 +36,103 @@
           </div>
         </div>
 
-        <div class="flex flex-col px-10 py-6 space-y-5 InfoBox">
-          <div class="flex flex-row w-full space-x-5">
+        <div
+          class="flex flex-col px-10 py-6 space-y-5 lg:flex-row lg:space-y-0 lg:space-x-5 InfoBox"
+        >
+          <div class="flex flex-row w-full space-x-5 lg:flex-col lg:space-x-0">
             <p class="text-5xl">+5</p>
             <p class="text-lg">Lorem Ipsum dolor</p>
           </div>
-          <div class="w-full h-1 separatorBar"></div>
-          <!-- Horizontal bar with #C940EB color -->
-          <div class="flex flex-row w-full space-x-5">
+          <div class="w-full h-1 lg:h-28 lg:w-5 xl:h-20 separatorBar"></div>
+          <div class="flex flex-row w-full space-x-5 lg:flex-col lg:space-x-0">
             <p class="text-5xl">60%</p>
             <p class="text-lg">Lorem Ipsum dolor</p>
           </div>
-          <div class="w-full h-1 separatorBar"></div>
-          <!-- Horizontal bar with #C940EB color -->
-          <div class="flex flex-row w-full space-x-5">
+          <div class="w-full h-1 lg:h-28 lg:w-5 xl:h-20 separatorBar"></div>
+
+          <div class="flex flex-row w-full space-x-5 lg:flex-col lg:space-x-0">
+            <p class="text-5xl">100</p>
+            <p class="text-lg">Lorem Ipsum dolor</p>
+          </div>
+        </div>
+      </div>
+      <div class="flex justify-end w-full lg:w-1/2">
+        <img
+          src="../assets/img/BannerRectangle.png"
+          alt="Banner image"
+          class="w-full max-w-2xl"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+.primaryButtons {
+  border-radius: 20px;
+  background: #c940eb;
+  box-shadow:
+    0px 5px 5px 0px #8c5dc9 inset,
+    0px 4px 50px 0px #c940eb;
+}
+.InfoBox {
+  border-radius: 30px;
+  background: #151417;
+  box-shadow:
+    0px 5px 5px 0px #8c5dc9 inset,
+    0px 4px 50px 0px #c940eb;
+}
+
+.separatorBar {
+  border-radius: 20px;
+  background: #c940eb;
+  box-shadow:
+    0px 5px 5px 0px #8c5dc9 inset,
+    0px 4px 50px 0px #c940eb;
+}
+</style> -->
+
+<template>
+  <div class="flex flex-col items-center justify-center min-h-screen">
+    <div class="flex flex-col items-center justify-center w-full md:flex-row">
+      <div class="flex flex-col items-center justify-center w-full lg:w-1/2">
+        <div class="w-full space-y-10 lg:w-4/5 2xl:w-1/2">
+          <h1 class="text-5xl">Hi there! I'm Daniel</h1>
+          <p class="text-2xl">
+            A front-end Web Developer passionate about creating interactive
+            applications and experiences on the web.
+          </p>
+
+          <div class="flex flex-row space-x-5 text-lg sm:space-x-10">
+            <button class="px-8 py-3 primaryButtons">LinkedIn</button>
+            <button class="px-5 py-3 primaryButtons">Icon</button>
+            <button class="px-5 py-3 primaryButtons">Icon</button>
+          </div>
+        </div>
+
+        <div
+          class="flex flex-col px-10 py-6 space-y-5 sm:flex-row sm:space-y-0 sm:space-x-5 InfoBox"
+        >
+          <div class="flex flex-row w-full space-x-5 sm:flex-col sm:space-x-0">
+            <p class="text-5xl">+5</p>
+            <p class="text-lg">Lorem Ipsum dolor</p>
+          </div>
+          <div class="w-full h-1 sm:h-28 sm:w-5 xl:h-20 separatorBar"></div>
+          <div class="flex flex-row w-full space-x-5 sm:flex-col sm:space-x-0">
+            <p class="text-5xl">60%</p>
+            <p class="text-lg">Lorem Ipsum dolor</p>
+          </div>
+          <div class="w-full h-1 sm:h-28 sm:w-5 xl:h-20 separatorBar"></div>
+
+          <div class="flex flex-row w-full space-x-5 sm:flex-col sm:space-x-0">
             <p class="text-5xl">100</p>
             <p class="text-lg">Lorem Ipsum dolor</p>
           </div>
