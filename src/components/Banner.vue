054c8a9539596e9d396c39596139856481be0938
danyczmartins <dani.martins2001@hotmail.com>
2023-09-14 16:23:22 +0100

    Mobile, XS and SM banner responsive part done

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -103,8 +103,10 @@ export default {
 <template>
   <div class="flex flex-col items-center justify-center min-h-screen">
     <div class="flex flex-col items-center justify-center w-full md:flex-row">
-      <div class="flex flex-col items-center justify-center w-full lg:w-1/2">
-        <div class="w-full space-y-10 lg:w-4/5 2xl:w-1/2">
+      <div
+        class="flex flex-col w-full md:items-center md:justify-center lg:w-1/2"
+      >
+        <div class="w-full pl-5 pr-5 space-y-10 lg:w-4/5 2xl:w-1/2">
           <h1 class="text-5xl">Hi there! I'm Daniel</h1>
           <p class="text-2xl">
             A front-end Web Developer passionate about creating interactive
@@ -118,25 +120,31 @@ export default {
           </div>
         </div>
 
+        <!-- ============================================== -->
+        <!-- md:-mr-5 md:translate-y-28 -->
         <div
-          class="flex flex-col px-10 py-6 space-y-5 sm:flex-row sm:space-y-0 sm:space-x-5 InfoBox"
+          class="flex flex-col px-10 py-6 space-y-5 InfoBox ml-5 mr-5 xs:mr-10 sm:flex-row sm:space-y-0 sm:space-x-5 sm:mr-5"
         >
           <div class="flex flex-row w-full space-x-5 sm:flex-col sm:space-x-0">
             <p class="text-5xl">+5</p>
             <p class="text-lg">Lorem Ipsum dolor</p>
           </div>
-          <div class="w-full h-1 sm:h-28 sm:w-5 xl:h-20 separatorBar"></div>
+
+          <div class="w-full h-1 sm:h-28 sm:w-5 separatorBar"></div>
+
           <div class="flex flex-row w-full space-x-5 sm:flex-col sm:space-x-0">
             <p class="text-5xl">60%</p>
             <p class="text-lg">Lorem Ipsum dolor</p>
           </div>
-          <div class="w-full h-1 sm:h-28 sm:w-5 xl:h-20 separatorBar"></div>
+
+          <div class="w-full h-1 sm:h-28 sm:w-5 separatorBar"></div>
 
           <div class="flex flex-row w-full space-x-5 sm:flex-col sm:space-x-0">
             <p class="text-5xl">100</p>
             <p class="text-lg">Lorem Ipsum dolor</p>
           </div>
         </div>
+        <!-- ============================================== -->
       </div>
       <div class="flex justify-end w-full lg:w-1/2">
         <img
