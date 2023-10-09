9e7f902fe85526b2431db5e30f727ee43984c0a6
danyczmartins <dani.martins2001@hotmail.com>
2023-10-09 18:56:09 +0100

    Backups

--- src/components/EndWrapper.vue
+++ src/components/EndWrapper.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex items-center justify-center w-full min-h-screen">
     <h1
-      class="text-center text-[#c940eb] [text-shadow:0_4px_50px_rgba(201,64,235,1)] text-[40px] lg:text-[70px] 2xl:text-[80px]"
+      class="text-center text-[#c940eb] [text-shadow:0_4px_50px_rgba(201,64,235,1)] text-[40px] lg:text-[70px] 2xl:text-[80px] hide"
     >
       &lt;/Wrapper&gt;
     </h1>
