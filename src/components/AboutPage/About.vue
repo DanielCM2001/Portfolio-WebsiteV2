fecf12b9923bb8f7635f6e007daf7279b0f9abc9
danyczmartins <dani.martins2001@hotmail.com>
2023-10-10 19:01:40 +0100

    about

--- src/components/AboutPage/About.vue
+++ src/components/AboutPage/About.vue
@@ -1,13 +1,68 @@
 <template>
-  <div>xd</div>
+  <div
+    class="relative flex items-center justify-center min-h-screen overflow-hidden"
+  >
+    <img
+      src="../../assets/img/DanielPhoto.png"
+      alt=""
+      class="absolute h-[350px] xs:h-[400px] rotate-90 transform -translate-y-1/4 top-[0%] z-0"
+    />
+    <div
+      class="w-full xs:w-9/12 lg:w-full lg:mx-[25vw] static-container mx-5 pt-[30vh] space-y-10"
+    >
+      <h1
+        class="text-[#c940eb] [text-shadow:0_4px_50px_rgba(201,64,235,1)] text-[40px]"
+      >
+        .about(Daniel)
+      </h1>
+
+      <p class="text-2xl">
+        A front-end Web Developer passionate about creating interactive
+        applications and experiences on the web.
+      </p>
+
+      <div class="flex flex-row space-x-5 text-lg sm:space-x-10">
+        <button class="px-8 py-3 primaryButtons">Download</button>
+        <button class="px-5 py-3 primaryButtons">xd</button>
+        <button class="px-5 py-3 primaryButtons">xd</button>
+      </div>
+
+      <div class="flex flex-col space-y-5">
+        <p class="text-2xl text-white">Main Skills</p>
+
+        <ul
+          class="grid gap-y-[1rem] md:grid-flow-col md:grid-cols-4 md:gap-x-[2rem]"
+        >
+          <li>Frontend, UX/UI</li>
+          <li>JavaScript, CSS, HTML, Vue, React, Tailwind</li>
+          <li>SQL, Solidity, Ethers.js, Cloud Computing, C#</li>
+          <li>Figma, Adobe XD</li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  setup() {
+  data() {
     return {};
   },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.primaryButtons {
+  border-radius: 20px;
+  background: #c940eb;
+  box-shadow:
+    0px 5px 5px 0px #8c5dc9 inset,
+    0px 4px 50px 0px #c940eb;
+}
+
+.static-container {
+  min-height: 100vh;
+  perspective: 900px;
+  perspective-origin: 50% 50vh;
+}
+</style>
