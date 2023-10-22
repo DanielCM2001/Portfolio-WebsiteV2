c499168ec730e2fee4ca02e3a4e20c292d1d4927
danyczmartins <dani.martins2001@hotmail.com>
2023-10-22 16:53:29 +0100

    space animated

--- src/components/back/NewComponents/SpaceAnimated2.vue
+++ src/components/back/NewComponents/SpaceAnimated2.vue
@@ -5,29 +5,13 @@
     >
       <div class="bottom-0 opacity-0 space"></div>
 
-      <img
-        src="../../assets/img/Ovni.gif"
-        alt=""
-        class="absolute top-0 left-0 z-30 ovni-hide ovni-ovni h-[150px] sm:h-[250px] 2xl:h-[300px]"
-      />
-
-      <div
-        class="absolute bottom-0 left-0 z-30 flex items-center justify-center min-w-full min-h-full rocket-wrapper"
-      >
-        <img
-          src="../../assets/img/Rocket.png"
-          alt=""
-          class="absolute h-[200px] bottom-[0%] z-10 sm:h-[300px] 2xl:h-[350px] rocket-rocket rocket-hide rocket"
-        />
-      </div>
-
       <div
         class="absolute bottom-0 left-0 z-30 flex items-center justify-center min-w-full min-h-full world-wrapper animatedWorld"
       >
         <img
           src="../../assets/img/WorldX4.png"
           alt=""
-          class="absolute h-[200px] bottom-[0%] z-10 sm:h-[300px] 2xl:h-[400px] world-world world-hide"
+          class="absolute h-[200px] bottom-[0%] z-10 world sm:h-[300px] 2xl:h-[400px] world-world world-hide"
         />
       </div>
       <h1
@@ -63,37 +47,6 @@ export default {
     };
   },
   mounted() {
-    const flightPath = {
-      curviness: 1.25,
-      autoRotate: false,
-      values: [
-        {
-          x: 0,
-          y: -50,
-        },
-        {
-          x: 0,
-          y: -100,
-        },
-        {
-          x: 0,
-          y: -150,
-        },
-        {
-          x: 0,
-          y: -200,
-        },
-        {
-          x: 0,
-          y: -250,
-        },
-        {
-          x: 0,
-          y: -window.innerHeight,
-        },
-      ],
-    };
-
     const tween = new TimelineLite();
 
     // First, the flight path animation for the rocket
@@ -128,16 +81,8 @@ export default {
             document
               .querySelector(".world-world")
               .classList.remove("world-show");
-            document
-              .querySelector(".rocket-rocket")
-              .classList.remove("rocket-show");
-            document.querySelector(".ovni-ovni").classList.remove("ovni-show");
           } else {
             document.querySelector(".world-world").classList.add("world-show");
-            document
-              .querySelector(".rocket-rocket")
-              .classList.add("rocket-show");
-            document.querySelector(".ovni-ovni").classList.add("ovni-show");
           }
         },
       })
@@ -147,13 +92,6 @@ export default {
       TweenLite.to(".world-wrapper", 1, {
         opacity: 1,
         ease: Linear.easeNone,
-      })
-    );
-
-    tween.add(
-      TweenLite.to(".rocket-wrapper", 1, {
-        bezier: flightPath,
-        ease: Linear.easeInOut,
       })
     );
 
@@ -171,7 +109,7 @@ export default {
     // Create a ScrollMagic scene
     const spaceScene = new ScrollMagic.Scene({
       triggerElement: ".spacecontainer",
-      duration: 3000, // Adjust the duration as needed
+      duration: 2000, // Adjust the duration as needed
       triggerHook: 0,
     })
       .setTween(tween)
@@ -231,23 +169,5 @@ export default {
 
 .world-show {
   transform: translateY(0);
-}
-
-.rocket-hide {
-  transform: translateY(100%);
-  transition: all 1s;
-}
-
-.rocket-show {
-  transform: translateY(0);
-}
-
-.ovni-hide {
-  transform: translateX(-100%);
-  transition: all 1s;
-}
-
-.ovni-show {
-  transform: translateX(0);
 }
 </style>
