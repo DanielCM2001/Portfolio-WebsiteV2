68092480df209551734d476c0a0463854753f2cb
danyczmartins <dani.martins2001@hotmail.com>
2023-11-15 16:01:18 +0000

    Update Banner.vue

--- src/components/HomePage/Banner.vue
+++ src/components/HomePage/Banner.vue
@@ -45,10 +45,11 @@
       />
     </div> -->
 <template>
+  <!-- lg:h-[100vh] -->
   <section
-    class="flex flex-col items-center justify-center h-[100vh] w-full border banner-section"
+    class="flex flex-col items-center justify-center min-h-screen w-full border banner-section pt-[70px]"
   >
-    <div
+    <!--  <div
       class="flex items-center justify-center w-full h-[100vh] text-center border"
     >
       <div class="flex flex-col items-center justify-center pt-20">
@@ -64,14 +65,94 @@
           class="z-0 w-full max-w-sm -mt-52 lg:max-w-2xl animatedText"
         />
       </div>
+    </div> -->
+    <!-- <h1 class="uppercase text-[36px] lg:text-[110px] z-10 animatedText">
+          I AM A FRONT END <br />
+          DEVELOPER
+        </h1>
+        <img
+          src="../../assets/img/BannerCircle.png"
+          alt="Banner image"
+          class="z-0 w-full max-w-sm -mt-20 xs:max-w-md lg:-mt-52 lg:max-w-2xl animatedText"
+        />
+        <img
+          src="../../assets/img/AvatarBigger.png"
+          alt="Banner image"
+          class="absolute z-20 w-full max-w-sm xs:max-w-md -mt-1 lg:mt-20 lg:max-w-[650px] animatedText"
+        /> --><!-- Might join the image above with the circle after, also need to fix the avatar borders below -->
 
-      <!-- flex flex-col items-center justify-center -->
+    <!--lg:text-[6vw]   circle sm:-mt-[20%] sm:max-w-[60%]-->
+
+    <!-- lg:h-[100vh] -->
+    <div class="flex items-center justify-center w-full text-center border">
+      <div class="flex flex-col items-center justify-center bg-black">
+        <h1
+          class="uppercase text-[9.5vw] 2md:text-[8vw] xl:text-[7vw] 2xl:text-[6vw] z-10"
+        >
+          I AM A FRONT END <br />
+          DEVELOPER
+        </h1>
+        <img
+          src="../../assets/img/BannerCircle.png"
+          alt="Banner image"
+          class="z-0 w-full max-w-[75%] -mt-[20%] lg:max-w-[65%]"
+        />
+      </div>
     </div>
+    <!-- ========================================== -->
+
+    <!-- lg:absolute lg:top-[65%] lg:w-1/3 -->
     <div
+      class="lg:absolute lg:top-[65%] lg:w-1/3 lg:left-[2vw] flex items-center justify-center w-full bg-gray-500 border"
+    >
+      <!-- mx-[4vw] lg:mx-[4vw] 2xl:mx-0 2xl:ml-[6vw] -->
+      <div class="flex flex-col mx-[4vw] w-full space-y-5 bg-gray-950">
+        <h1 class="text-3xl 3xl:text-[1.5vw] 3xl:leading-[2vw]">
+          Hi there! I'm <span class="text-[#C3023A]">Daniel</span>
+        </h1>
+        <!-- THE VW IS NOT WORKING WHEN 4K, FIX IT TOMORROW -->
+        <!-- 3xl:text-[1.5vw] -->
+
+        <!-- text-[6.5vw] xs:text-[4.5vw] -->
+        <!-- 3xl:text-[1.5vw] 3xl:leading-[2vw] -->
+        <h1 class="text-2xl 3xl:text-[1.25vw] 3xl:leading-[1.65vw]">
+          Passionate about creating interactive applications and
+          <span class="text-[#C3023A]">experiences on the web.</span>
+        </h1>
+
+        <div
+          class="flex flex-col py-5 space-y-5 sm:flex-row sm:space-y-0 sm:space-x-5"
+        >
+          <div class="flex flex-row items-center w-full space-x-5">
+            <p
+              class="text-5xl 3xl:text-[2.5vw] 3xl:leading-[3vw] text-[#C3023A]"
+            >
+              +8
+            </p>
+            <p class="text-lg 3xl:text-[0.95vw] 3xl:leading-[1.3vw]">
+              Projects completed
+            </p>
+          </div>
+
+          <div class="flex flex-row items-center w-full space-x-5">
+            <p
+              class="text-5xl 3xl:text-[2.5vw] 3xl:leading-[3vw] text-[#C3023A]"
+            >
+              2
+            </p>
+            <p class="text-lg 3xl:text-[0.95vw] 3xl:leading-[1.3vw]">
+              Years of Experience
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- <div
       class="lg:absolute lg:top-[65%] left-0 flex items-center justify-center lg:w-1/3 bg-gray-500 border w-full"
     >
       <div
-        class="flex flex-col ml-[2vw] 2xl:ml-[6vw] w-full bg-gray-950 space-y-5"
+        class="flex flex-col mx-[4vw] 2xl:mx-0 2xl:ml-[6vw] w-full bg-gray-950 space-y-5"
       >
         <h1 class="text-3xl">
           Hi there! I'm <span class="text-[#C3023A]">Daniel</span>
@@ -84,18 +165,18 @@
         <div
           class="flex flex-col py-5 space-y-5 sm:flex-row sm:space-y-0 sm:space-x-5 max-w-7xl"
         >
-          <div class="flex flex-row w-full space-x-5">
+          <div class="flex flex-row items-center w-full space-x-5">
             <p class="text-5xl md:text-4xl lg:text-5xl text-[#C3023A]">+8</p>
             <p class="text-lg md:text-md lg:text-lg">Projects completed</p>
           </div>
 
-          <div class="flex flex-row w-full space-x-5">
+          <div class="flex flex-row items-center w-full space-x-5">
             <p class="text-5xl md:text-4xl lg:text-5xl text-[#C3023A]">2</p>
             <p class="text-lg md:text-md lg:text-lg">Years of Experience</p>
           </div>
         </div>
       </div>
-    </div>
+    </div> -->
   </section>
 </template>
 
