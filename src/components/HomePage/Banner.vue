d43b397b95dffef6ec47f8e8c48ea343c0d6603b
danyczmartins <dani.martins2001@hotmail.com>
2023-12-13 11:48:16 +0000

    Banner

--- src/components/HomePage/Banner.vue
+++ src/components/HomePage/Banner.vue
@@ -62,7 +62,7 @@
   </section>
 </template> -->
 
-   <!-- <div class="flex justify-center items-center h-[60%]">
+<!-- <div class="flex justify-center items-center h-[60%]">
           <div class="flex flex-col items-center justify-center">
             <img
               src="../../assets/img/ArrowRight.png"
@@ -155,68 +155,99 @@
   </section>
 </template> -->
 
-
 <template>
   <section
-    class="flex flex-row items-center justify-center min-h-screen w-full pt-[80px] px-[4vw] "
+    class="flex flex-row items-center justify-center min-h-screen w-full pt-[80px] px-[4vw]"
   >
-    <div class="z-10 flex items-stretch w-full flex-col md:flex-row  md:space-x-[0%]  ">
-      <div class="flex flex-col justify-center break-words md:break-normal w-full md:w-[70%] z-10 ">
+    <div
+      class="z-10 flex items-stretch w-full flex-col md:flex-row md:space-x-[0%]"
+    >
+      <div
+        class="flex flex-col justify-center break-words md:break-normal w-full md:w-[70%] z-10"
+      >
         <h1 class="text-[11.2vw] xs:text-[36px] secondaryFont text-[#f5eee6]">
           Hi there! I'm Daniel
         </h1>
 
         <!-- overflow-hidden  -->
         <!-- overflow-y-clip  -->
-        <div class="overflow-y-clip ">
-            <h1 class="text-[14.4vw] md:text-[10.4vw] 2md:text-[90px] xl:text-[110px] 2xl:text-[140px] uppercase secondaryFont text-[#F5EEE6] hideBannerTitle ">
-            Designer <span class="text-[9.3vw] md:text-[35px] xl:text-[45px] 2xl:text-[64px] ">&</span><br />
+        <div class="overflow-y-clip">
+          <h1
+            class="text-[14.4vw] md:text-[10.4vw] 2md:text-[90px] xl:text-[110px] 2xl:text-[140px] uppercase secondaryFont text-[#F5EEE6] hideBannerTitle"
+          >
+            Designer
+            <span
+              class="text-[9.3vw] md:text-[35px] xl:text-[45px] 2xl:text-[64px]"
+              >&</span
+            ><br />
             Developer
           </h1>
         </div>
 
         <!-- 2md:text-[45px] -->
         <!-- 2md:text-[40px] -->
-         <h1 class="text-[12.5vw] xs:text-[40px] text-[#f5eee6] hideBannerSubtitle">
+        <h1
+          class="text-[12.5vw] xs:text-[40px] text-[#f5eee6] hideBannerSubtitle"
+        >
           +2
           <span class="text-[11.2vw] xs:text-[36px]">Years of Experience</span>
         </h1>
-        <h1 class="text-[12.5vw] xs:text-[40px]  text-[#f5eee6] hideBannerSubtitle">
+        <h1
+          class="text-[12.5vw] xs:text-[40px] text-[#f5eee6] hideBannerSubtitle"
+        >
           +7
           <span class="text-[11.2vw] xs:text-[36px]">Projects Completed</span>
-        </h1> 
+        </h1>
       </div>
 
-      <div class="break-words flex flex-col w-full md:w-[30%]    ">
-      
+      <div class="break-words flex flex-col w-full md:w-[30%]">
         <!-- justify-center items-stretch -->
-         <div class="flex flex-row md:flex-row h-[50%] relative py-5 md:-ml-10 ">
-
+        <div class="flex flex-row md:flex-row h-[50%] relative py-5 md:-ml-10">
           <!-- Arrow right -->
-            <div class="w-1/2 flex flex-col items-center justify-end">
-              <img
+          <div class="w-1/2 flex flex-col items-center justify-end">
+            <img
               src="../../assets/img/ArrowRight.png"
               alt=""
-              class="max-w-[80%] md:max-w-[100%] "
-              />
-              <h1 class="text-[10vw] xs:text-[32px] md:text-[30px] xl:text-[32px] line-through ">Click here</h1>
-            </div>
+              class="max-w-[80%] md:max-w-[100%]"
+            />
+            <h1
+              class="text-[10vw] xs:text-[32px] md:text-[30px] xl:text-[32px] line-through"
+            >
+              Click here
+            </h1>
+          </div>
 
-            <!-- Lightbulb -->
-            <div class="w-1/2 flex items-center justify-center">
-              <img
-                  src="../../assets/img/LightBulb.png"
-                  alt=""
-                  class="max-w-[100%] max-h-[100%] z-10 rotate-180 md:rotate-0 "
-              />
-              <!-- <div
+          <!-- Lightbulb -->
+          <!-- rotate-180 md:rotate-0 -->
+          <div class="w-1/2 flex items-center justify-center">
+            <img
+              src="../../assets/img/LightBulb.png"
+              alt=""
+              class="max-w-[100%] max-h-[100%] z-10 rotate-0 mr-[1px]"
+            />
+
+            <!-- w-[14px] -->
+            <div
+              class="w-[14px] h-[20%] -top-[0%] md:h-[150%] md:-top-[100%] bg-[#b9acac] absolute z-0 border-[#201a27] border-[3px]"
+            ></div>
+            <!-- w-[24%] right-[0vw] -->
+            <!-- w-[20%] right-[4vw] -->
+            <!-- w-[25.9vw] xs:w-[26.2vw] sm:w-[26.4vw] -right-[4vw] -->
+
+            <!--   style="width: calc(25vw + 1.5%)" -->
+            <!-- -right-[4vw] -->
+            <div
+              class="-right-[4vw] h-[14px] -top-[0%] bg-[#b9acac] absolute z-0 border-[#201a27] border-t-[3px] border-b-[3px] md:hidden"
+              style="width: calc(25% + 4vw - 4px)"
+            ></div>
+
+            <!-- <div
                 class="w-[14px] h-[150%] -top-[100%] bg-[#b9acac] absolute  z-0 border-[#201a27] border-[3px]"
                ></div>  -->
-            </div>
-        </div> 
-       
+          </div>
+        </div>
 
-     <!--    <div class="py-10  space-y-5 h-[50%] md:px-10 md:-ml-44 md:mt-52 xl:mt-20 xl:-ml-20 2xl:mt-20 2xl:ml-0 3xl:mt-0 MichaelAngeloBackground">
+        <!--    <div class="py-10  space-y-5 h-[50%] md:px-10 md:-ml-44 md:mt-52 xl:mt-20 xl:-ml-20 2xl:mt-20 2xl:ml-0 3xl:mt-0 MichaelAngeloBackground">
           <h1 class="text-[12.5vw] xs:text-[40px] md:text-[3.9vw] 2md:text-[36px] 2xl:text-[40px] ">
             Passionate about creating interactive applications and experiences
             on the web.
@@ -230,30 +261,35 @@
               class="h-[40px] w-[35px] -mt-1"
             />
           </div>
-        </div>  --> 
+        </div>  -->
 
-      
-        <div class=" h-[50%]  md:-ml-44 md:mt-52 xl:mt-20 xl:-ml-20 2xl:mt-20 2xl:ml-0 3xl:mt-0   ">
-            <div class="hideBannerImage   flex justify-center flex-col ">
-               <div class="MichaelAngeloBackground md:px-10 py-10 space-y-5">
-
-          <h1 class="text-[12.5vw] xs:text-[40px] md:text-[3.9vw] 2md:text-[36px] 2xl:text-[40px] ">
-            Passionate about creating interactive applications and experiences
-            on the web.
-          </h1>
+        <div
+          class="h-[50%] md:-ml-44 md:mt-52 xl:mt-20 xl:-ml-20 2xl:mt-20 2xl:ml-0 3xl:mt-0"
+        >
+          <div class="hideBannerImage flex justify-center flex-col">
+            <div class="MichaelAngeloBackground md:px-10 py-10 space-y-5">
+              <h1
+                class="text-[12.5vw] xs:text-[40px] md:text-[3.9vw] 2md:text-[36px] 2xl:text-[40px]"
+              >
+                Passionate about creating interactive applications and
+                experiences on the web.
+              </h1>
 
               <div class="flex flex-row items-center space-x-5">
-                <h1 class="text-[10vw] xs:text-[32px] md:text-[30px] xl:text-[32px] line-through">LET'S GET IN CONTACT</h1>
+                <h1
+                  class="text-[10vw] xs:text-[32px] md:text-[30px] xl:text-[32px] line-through"
+                >
+                  LET'S GET IN CONTACT
+                </h1>
                 <img
                   src="../../assets/img/GithubIcon.png"
                   alt=""
                   class="h-[40px] w-[35px] -mt-1"
                 />
               </div>
-           </div>  
-          </div>  
+            </div>
+          </div>
         </div>
-
       </div>
     </div>
   </section>
@@ -262,7 +298,7 @@
 <script>
 export default {
   mounted() {
-     /* const observer = new IntersectionObserver((entries) => {
+    /* const observer = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
           entry.target.classList.add("showBannerTitle");
@@ -298,11 +334,10 @@ export default {
     hiddenElementsY.forEach((el) => observer.observe(el));
 
     const hiddenElementsX = document.querySelectorAll(".hideNavBar");
-    hiddenElementsX.forEach((el) => observer.observe(el)); */ 
-
+    hiddenElementsX.forEach((el) => observer.observe(el)); */
 
     /* THIS ONE'S WORKING */
-/*      const observer = new IntersectionObserver((entries) => {
+    /*      const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
       entry.target.classList.add("showBannerTitle");
@@ -355,64 +390,75 @@ export default {
     hiddenElementsY.forEach((el) => observer.observe(el));  */
 
     const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("showBannerTitle");
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("showBannerTitle");
 
-      if (entry.target.clientHeight > 0) {
-        // Flag to track whether showBannerTitle animation has completed
-        let titleAnimationComplete = false;
+          if (entry.target.clientHeight > 0) {
+            // Flag to track whether showBannerTitle animation has completed
+            let titleAnimationComplete = false;
 
-        // Flag to track whether showNavBar animation has completed
-        let navBarAnimationComplete = false;
+            // Flag to track whether showNavBar animation has completed
+            let navBarAnimationComplete = false;
 
-        let subtitleAnimationComplete = false;
+            let subtitleAnimationComplete = false;
 
+            // Function to handle completion of showBannerTitle animation
+            const onTitleAnimationEnd = () => {
+              titleAnimationComplete = true;
 
+              // Check if both animations have completed
+              if (titleAnimationComplete && navBarAnimationComplete) {
+                entry.target.classList.add("showBannerSubtitle");
+              }
+            };
 
-        // Function to handle completion of showBannerTitle animation
-        const onTitleAnimationEnd = () => {
-          titleAnimationComplete = true;
+            // Function to handle completion of showNavBar animation
+            const onNavBarAnimationEnd = () => {
+              navBarAnimationComplete = true;
 
-          // Check if both animations have completed
-          if (titleAnimationComplete && navBarAnimationComplete ) {
-            entry.target.classList.add("showBannerSubtitle");
+              // Check if both animations have completed
+              if (titleAnimationComplete && navBarAnimationComplete) {
+                entry.target.classList.add("showBannerSubtitle");
+              }
+            };
+
+            const onSubtitleAnimationEnd = () => {
+              subtitleAnimationComplete = true;
+
+              // Check if both animations have completed
+              if (
+                titleAnimationComplete &&
+                navBarAnimationComplete &&
+                subtitleAnimationComplete
+              ) {
+                entry.target.classList.add("showBannerImage");
+              }
+            };
+
+            // Add event listener for the 'transitionend' event of showBannerTitle
+            entry.target.addEventListener(
+              "transitionend",
+              onTitleAnimationEnd,
+              { once: true }
+            );
+
+            // Add event listener for the 'transitionend' event of showNavBar
+            entry.target.addEventListener(
+              "transitionend",
+              onNavBarAnimationEnd,
+              { once: true }
+            );
+
+            entry.target.addEventListener(
+              "transitionend",
+              onSubtitleAnimationEnd,
+              { once: true }
+            );
           }
-        };
-
-        // Function to handle completion of showNavBar animation
-        const onNavBarAnimationEnd = () => {
-          navBarAnimationComplete = true;
-
-          // Check if both animations have completed
-          if (titleAnimationComplete && navBarAnimationComplete ) {
-            entry.target.classList.add("showBannerSubtitle");
-          }
-        };
-
-        const onSubtitleAnimationEnd = () => {
-          subtitleAnimationComplete = true;
-
-          // Check if both animations have completed
-          if (titleAnimationComplete && navBarAnimationComplete && subtitleAnimationComplete) {
-            entry.target.classList.add("showBannerImage");
-          }
-        };
-
-      
-
-        // Add event listener for the 'transitionend' event of showBannerTitle
-        entry.target.addEventListener("transitionend", onTitleAnimationEnd, { once: true });
-
-        // Add event listener for the 'transitionend' event of showNavBar
-        entry.target.addEventListener("transitionend", onNavBarAnimationEnd, { once: true });
-
-        entry.target.addEventListener("transitionend", onSubtitleAnimationEnd, { once: true });
-
-      }
-    }
-  });
-});
+        }
+      });
+    });
 
     const hiddenElements = document.querySelectorAll(".hideBannerTitle");
     hiddenElements.forEach((el) => observer.observe(el));
@@ -425,7 +471,6 @@ export default {
 
     const hiddenElementsZ = document.querySelectorAll(".hideBannerImage");
     hiddenElementsZ.forEach((el) => observer.observe(el));
-   
   },
 };
 </script>
